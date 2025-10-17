778b0dc72d6131eb67285bb1ab50b6b66d8dd954
derekmatins51-blip <derekmatins51@gmail.com>
2025-10-16 20:26:36 -0700

    Fixed npm run dev error

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,39 @@
 const mix = require("laravel-mix");
 
+const path = require('path');
+
+mix.options({
+    processCssUrls: false
+});
+
+mix.webpackConfig({
+    resolve: {
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "fs": false,  // Disable fs as it's server-only
+            "path": require.resolve("path-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            "net": false,
+            "tls": false,
+            "constants": require.resolve("constants-browserify"),
+            "assert": require.resolve("assert"),
+            "buffer": require.resolve("buffer"),
+            "url": require.resolve("url"),
+            "util": require.resolve("util")
+        },
+        alias: {
+            '@': path.resolve('resources/js'),
+        }
+    },
+    externals: {
+        // Exclude server-only SDKs from frontend bundle
+        'metaapi.cloud-sdk': 'MetaApiCloudSdk'
+    }
+});
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
